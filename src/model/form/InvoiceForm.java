07f2198afe24bae0dd91ba2a6051bc33777b2b31
phn210 <57532941+phn210@users.noreply.github.com>
2021-01-06 14:34:42 +0700

    Invoice Tab

--- src/model/form/InvoiceForm.java
+++ src/model/form/InvoiceForm.java
@@ -1,4 +1,135 @@
 package model.form;
 
+import model.entity.*;
+
+import java.sql.Date;
+
 public class InvoiceForm {
+    private int id;
+    private Date date;
+    private String customerName;
+    private String customerPhone;
+    private String customerAddress;
+    private String employee;
+    private String paymentMethod;
+    private Double totalMoney;
+    private Double tax;
+    private Double surcharge;
+    private String note;
+    private String branch;
+
+    public InvoiceForm(){
+
+    }
+
+    public InvoiceForm(Invoice invoice, Customer customer, Employee employee, Branch branch){
+        this.setId(invoice.getId());
+        this.setDate(invoice.getDate());
+        this.setCustomerName(customer.getName());
+        this.setCustomerPhone(customer.getPhone());
+        this.setCustomerAddress(customer.getAddress());
+        this.setEmployee(employee.getName());
+        this.setPaymentMethod(invoice.getPaymentMethod());
+        this.setTotalMoney(invoice.getTotalMoney());
+        this.setTax(invoice.getTax());
+        this.setSurcharge(invoice.getSurcharge());
+        this.setNote(invoice.getNote());
+        this.setBranch(branch.getName());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public String getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(String employee) {
+        this.employee = employee;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public Double getTotalMoney() {
+        return totalMoney;
+    }
+
+    public void setTotalMoney(Double totalMoney) {
+        this.totalMoney = totalMoney;
+    }
+
+    public Double getTax() {
+        return tax;
+    }
+
+    public void setTax(Double tax) {
+        this.tax = tax;
+    }
+
+    public Double getSurcharge() {
+        return surcharge;
+    }
+
+    public void setSurcharge(Double surcharge) {
+        this.surcharge = surcharge;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
 }
--- src/service/invoice/InvoicesService.java
+++ src/service/invoice/InvoicesService.java
@@ -1,4 +1,38 @@
 package service.invoice;
 
+import model.entity.*;
+import model.form.InvoiceForm;
+import repository.*;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class InvoicesService {
+    public InvoiceRepo invoiceRepo;
+    public InvoiceDetailRepo invoiceDetailRepo;
+    public CustomerRepo customerRepo;
+    public EmployeeRepo employeeRepo;
+    public BranchRepo branchRepo;
+
+    public InvoicesService(){
+        this.invoiceRepo = new InvoiceRepo();
+        this.invoiceDetailRepo = new InvoiceDetailRepo();
+        this.customerRepo = new CustomerRepo();
+        this.employeeRepo = new EmployeeRepo();
+        this.branchRepo = new BranchRepo();
+    }
+
+    public ArrayList<InvoiceForm> getAllInvoiceForm() throws SQLException, NullPointerException {
+        ArrayList<Invoice> invoices = invoiceRepo.findAll();
+        ArrayList<InvoiceForm> invoiceForms = new ArrayList<>();
+        for(Invoice invoice: invoices){
+            Customer customer = customerRepo.findByID(invoice.getCustomerID());
+            Employee employee = employeeRepo.findById(invoice.getEmployeeID());
+            Branch branch = branchRepo.findById(employee.getBranchID());
+            InvoiceForm invoiceForm = new InvoiceForm(invoice, customer, employee, branch);
+            invoiceForms.add(invoiceForm);
+        }
+        return invoiceForms;
+    }
+
 }
